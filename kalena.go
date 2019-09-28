c6228b3ba91b1539df7df255a8904d6130500d3e
Bailey <bsy2658@gmail.com>
2019-09-28 16:17:41 +0900

    에러처리를 메소드 형태로 변경함.

--- kalena.go
+++ kalena.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
 var (
@@ -22,40 +21,11 @@ var (
 
 func main() {
 	flag.Parse()
-
 	if !*flagAdd {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	if *flagTitle == "" || *flagLayerTitle == "" || *flagStart == "" || *flagEnd == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	if *flagLayerColor != "" {
-		if !regexWebColor.MatchString(*flagLayerColor) {
-			log.Fatal("#FF0011 형식의 문자열이 아닙니다")
-		}
-	}
 
-	//사용자에게 입력받은 데이터값이 유효한지 체크
-	if !regexRFC3339Time.MatchString(*flagStart) {
-		log.Fatal("2019-09-09T14:43:34+09:00 형식의 문자열이 아닙니다")
-	}
-	if !regexRFC3339Time.MatchString(*flagEnd) {
-		log.Fatal("2019-09-09T14:43:34+09:00 형식의 문자열이 아닙니다")
-	}
-	startTime, err := time.Parse("2006-01-02T15:04:05-07:00", *flagStart)
-	if err != nil {
-		log.Fatal(err)
-	}
-	endTime, err := time.Parse("2006-01-02T15:04:05-07:00", *flagEnd)
-	if err != nil{
-		log.Fatal(err)
-	}
-	// end가 start 시간보다 큰지 체크하는 부분
-	if !checkTime(startTime, endTime) {
-		log.Fatal("끝시간이 시작시간보다 작습니다")
-	}
 	// declare variables for struct
 	c := Calendar{}
 	l := Layer{}
@@ -65,9 +35,20 @@ func main() {
 	l.Color = *flagLayerColor
 	l.Hidden = *flagLayerHidden
 	l.Greyscale = *flagLayerGreyscale
+
 	s.Title = *flagTitle
 	s.Start = *flagStart
 	s.End = *flagEnd
+
+	// 체크 구문
+	err := l.CheckError()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.CheckError()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	l.Schedules = append(l.Schedules, s)
 	c.Layers = append(c.Layers, l)
