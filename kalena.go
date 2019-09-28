647f9d705b1e04171be16ee128d383b793201e84
Bailey <bsy2658@gmail.com>
2019-09-28 17:23:41 +0900

    웹서버 만듦

--- kalena.go
+++ kalena.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -17,41 +18,47 @@ var (
 	flagStart          = flag.String("start", "", "Start time")
 	flagEnd            = flag.String("end", "", "End time")
 	flagLocation       = flag.String("location", "Asia/Seoul", "location name")
+
+	flagHTTPPort = flag.String("http", "", "Web Service Port Number")
 )
 
 func main() {
 	flag.Parse()
-	if !*flagAdd {
+	if *flagAdd {
+		c := Calendar{}
+		l := Layer{}
+		s := Schedule{}
+
+		l.Title = *flagLayerTitle
+		l.Color = *flagLayerColor
+		l.Hidden = *flagLayerHidden
+		l.Greyscale = *flagLayerGreyscale
+
+		s.Title = *flagTitle
+		s.Start = *flagStart
+		s.End = *flagEnd
+
+		// 체크 구문
+		err := l.CheckError()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = s.CheckError()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		l.Schedules = append(l.Schedules, s)
+		c.Layers = append(c.Layers, l)
+
+		fmt.Println(c)
+	} else if *flagHTTPPort != "" {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("kalena 1,2,3...31"))
+		})
+		http.ListenAndServe(*flagHTTPPort, nil)
+	} else {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-
-	// declare variables for struct
-	c := Calendar{}
-	l := Layer{}
-	s := Schedule{}
-
-	l.Title = *flagLayerTitle
-	l.Color = *flagLayerColor
-	l.Hidden = *flagLayerHidden
-	l.Greyscale = *flagLayerGreyscale
-
-	s.Title = *flagTitle
-	s.Start = *flagStart
-	s.End = *flagEnd
-
-	// 체크 구문
-	err := l.CheckError()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = s.CheckError()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	l.Schedules = append(l.Schedules, s)
-	c.Layers = append(c.Layers, l)
-
-	fmt.Println(c)
 }
