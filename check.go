37514ef27824abf22ddc986a27d3d89be65dfad7
Bailey <bsy2658@gmail.com>
2019-10-23 01:02:45 +0900

    시간도 레귤러익스프레션으로 체크

--- check.go
+++ check.go
@@ -5,6 +5,6 @@ import (
 )
 
 var (
-	regexRFC3339Time = regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])T\d{2}:\d{2}:\d{2}[-+]\d{2}:\d{2}$`) //2019-09-09T02:46:52+09:00
-	regexWebColor    = regexp.MustCompile(`^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`)                                                  //#FF0011, #ff0011, #F01, #f01
+	regexRFC3339Time = regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9][-+]\d{2}:\d{2}$`) //2019-09-09T02:46:52+09:00
+	regexWebColor    = regexp.MustCompile(`^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$`)                                                                          //#FF0011, #ff0011, #F01, #f01
 )
