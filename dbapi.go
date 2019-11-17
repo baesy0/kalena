aeee0f7d6096eb6a9f5be2af6d52b1e24e01d10f
Bailey <bsy2658@gmail.com>
2019-11-17 17:13:09 +0900

    입력받은 년, 월에 해당하는 스케쥴을 가져오는 함수

--- dbapi.go
+++ dbapi.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -26,4 +29,45 @@ func allSchedules(session *mgo.Session, Collection string) ([]Schedule, error) {
 		return result, err
 	}
 	return result, nil
+}
+
+// SearchMonth 함수는 Collection, Year, Month을 입력받아 start의 값이 일치하면 반환한다.
+func SearchMonth(session *mgo.Session, Collection, year, month string) ([]Schedule, error) {
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB(*flagDBName).C(Collection)
+	var all []Schedule
+	var results []Schedule
+	err := c.Find(bson.M{}).All(&all)
+	if err != nil {
+		return nil, err
+	}
+	for _, s := range all {
+		startTime, err := time.Parse("2006-01-02T15:04:05-07:00", s.Start)
+		if err != nil {
+			return []Schedule{}, err
+		}
+		endTime, err := time.Parse("2006-01-02T15:04:05-07:00", s.End)
+		if err != nil {
+			return []Schedule{}, err
+		}
+		// 현재는 한국 시간으로 한다. 사용자별로 시간대를 설정할 수 있는 기능은 나중에 만들겠다.
+		monthStart, err := time.Parse("2006-01-02T15:04:05-07:00", fmt.Sprintf("%s-%s-01T00:00:00+09:00", year, month))
+		if err != nil {
+			return []Schedule{}, err
+		}
+		monthEnd, err := time.Parse("2006-01-02T15:04:05-07:00", fmt.Sprintf("%s-%s-30T00:00:00+09:00", year, month))
+		if err != nil {
+			return []Schedule{}, err
+		}
+		// endTime 이 monthStart 보다 작을 때 제외한다.
+		if endTime.Before(monthStart) {
+			continue
+		}
+		// startTime 이 monthEnd 보다 클 때 제외한다.
+		if startTime.After(monthEnd) {
+			continue
+		}
+		results = append(results, s)
+	}
+	return results, nil
 }
