577953c7dd756637e9c9ba334680a1141133a42a
Bailey <bsy2658@gmail.com>
2020-01-03 19:37:42 +0900

    close: #142

--- assets/js/kalena.js
+++ assets/js/kalena.js
@@ -60,13 +60,19 @@ document.getElementById("calendar").onmousemove = function () {
 //올해, 이번달 페이지일 경우, 오늘 날짜에 동그라미를 그린다.
 window.onload = function highlightToday(){
     let c = document.getElementById("calendar")
-    let qYear = parseInt(c.dataset.queryyear);
-    let qMonth = parseInt(c.dataset.querymonth);
     let offset = parseInt(c.dataset.offset);
-    let today = new Date();
-    
-    if(qYear == today.getFullYear() && qMonth == (today.getMonth()+1)){
-        document.getElementById(offset + today.getDate()-1).style.backgroundColor = "red";
-        console.log(document.getElementById(offset + today.getDate()-1).outerHTML);
+    let day = new Date();
+    let year = day.getFullYear().toString();
+    let month = (day.getMonth() + 1).toString();
+    let date = day.getDate().toString();
+    if(month.length == 1){
+        month = '0' + month;//m->mm
+    }
+    if(date.length == 1){//d->dd
+        date = '0' + date;
+    }
+    let today = year + '-' + month + '-' + date;//yyyy-mm-dd
+    if(document.getElementById(offset + day.getDate() -1).getAttribute('value') == today){
+        document.getElementById(offset + day.getDate() -1).style.backgroundColor="red"
     }
 }
