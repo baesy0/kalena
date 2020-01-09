e4a077c554aa0871549539a560e3d5c8cc6f42ea
Bailey <bsy2658@gmail.com>
2020-01-09 19:12:39 +0900

    close: #126 모달을 통해 입력한 스케쥴 DB에 저장

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,18 +1,29 @@
 function addScheduleModal(){
     let startdate = document.getElementById("startdate").value;
     let enddate = document.getElementById("enddate").value;
+    let starttime = document.getElementById("starttime").value;
+    let endtime = document.getElementById("endtime").value;
     let title = document.getElementById("title").value;
     let layer = document.getElementById("layer").value;
+
+    //나중에 사용자가 지정한 지역의 시간이 들어가도록 해야한다. 일단 한국시간(KST)으로 설정해둠.(UTC기준 +08:00)
+    let start = startdate + "T" + starttime + ":00+08:00";
+    let end = enddate + "T" + endtime + ":00+08:00";
+
+    console.log(start);
+    console.log(end);
 
     $.ajax({
         url:"/api/schedule",
         type: "post",
         data:{
             collection: "bae",
-            start: startdate,
-            end: enddate,
             title: title,
+            start: start,
+            end: end,
+            color: "#f5ce42",
             layer: layer,
+            hidden: "false"
         },
         dataType: "json",
         success: function(data){
